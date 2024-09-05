b48abc56edbb7981dfe7f21155f273c3bcc5e57b
killggon <nandbhandari0662@gmail.com>
2024-09-06 04:21:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY The_Fiend102
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("YOUR BOT TOKEN")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("The_Fiend102")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("TELEGRAM ID")
+
+
+
+
+
+
+
+  
